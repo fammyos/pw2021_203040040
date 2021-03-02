1209716275f17b8e9ccaa50803ca868b841061da
fammyos <75975672+fammyos@users.noreply.github.com>
2021-03-03 00:13:08 +0700

    Update tugas1.php

--- praktikum/tugas1/tugas1.php
+++ praktikum/tugas1/tugas1.php
@@ -4,38 +4,6 @@
     // Shift Praktikum : Jum'at Pukul 10.00
 ?>
 
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas 1</title>
-    <style>
-        td {
-            background-color: blue;
-        }
-    </style>
-</head>
-<body>
-    <table width = "200px" cellspacing = "5px" cellpadding = "0px" border="1px"></table>
-
-    <?php for( $baris = 1; $baris <= 6; $baris++) : ?>
-        <tr>
-            <?php for( $kolom = 1; $kolom <= 6; $kolom++) : ?>
-                <?php $keduanya = $baris + $kolom; ?>
-                    <?php if( $keduanya % 2 == 1) : ?>
-                        <td height = 50px width = 50px bgcolor = red></td>
-                    <?php else : ?>
-                        <td height = 50px width = 50px bgcolor = red></td>
-                    <?php endif; ?>    
-            <?php endfor; ?>
-        </tr>
-    <?php endfor; ?>
-
-</body>
-</html> -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
